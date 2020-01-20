71165117da4e22657f0fce14b2a70d85b5534b96
zubayerhimel <zhimel007@gmail.com>
2020-01-21 02:24:05 +0600

    added solution of string rotation

---
+++ Array and Strings/String/StringRotation/StringRotation.js
@@ -0,0 +1,12 @@
+const stringRotation = (firstString, secondString) =>
+{
+    if(firstString.length !== secondString.length)
+    {
+        return false
+    }
+
+    return (firstString += firstString).includes(secondString)
+}
+
+console.log(stringRotation('abcd', 'bdca')) // false
+console.log(stringRotation('abcd', 'bcda')) // true
