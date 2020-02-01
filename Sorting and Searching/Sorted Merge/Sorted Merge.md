f3e16bfe21a42eb609d5daa64c60e3529fdd5a26
zubayerhimel <zhimel007@gmail.com>
2020-02-01 17:46:08 +0600

    solution of sorted merge

---
+++ Sorting and Searching/Sorted Merge/Sorted Merge.md
@@ -0,0 +1,11 @@
+**Contents**
+
+- [Question](#question)
+- [Solution and Explanation in JavaScript](#solution-and-explanation-in-javascript)
+- [Solution and Explanation in CPP](#solution-and-explanation-in-cpp)
+  - [Functions](#functions)
+- [Complexity](#complexity)
+  
+## Question 
+*You are given two sorted array **A** and **B** where **A** has a large enough buffer at the end to hold **B**. Write a method to merge **B** into **A** in sorted order*
+
---
+++ Sorting and Searching/Sorted Merge/SortedMerge.js
@@ -0,0 +1,35 @@
+const SortedMerge = (array1, array2, firstArrayElements, secondArrayElements) =>
+{
+    let i = firstArrayElements - 1, j = secondArrayElements - 1
+    let lastIndex = firstArrayElements + secondArrayElements - 1
+
+    while (j >= 0)
+    {
+        if (i >= 0 && array1[i] > array2[j])
+        {
+            array1[lastIndex] = array1[i]
+            i--
+        }
+        else
+        {
+            array1[lastIndex] = array2[j]
+            j--
+        }
+        lastIndex--
+    }
+}
+
+const PrintArray = (array3, limit) =>
+{
+    for (let i = 0; i < limit; i++)
+    {
+        console.log(`${array3[i]} `)
+        }
+    }
+
+
+let array1 = [10, 12, 13, 14, 16, NaN, NaN, NaN, NaN, NaN]
+let array2 = [5, 6, 7, 8, 9]
+let n = 5, m = 5, finalArraySize = 10
+SortedMerge(array1, array2, n, m)
+PrintArray(array1, finalArraySize)
