63acb97e3ca2b1aa5677f5653aa1e4a6c797c86a
zubayerhimel <zhimel007@gmail.com>
2020-02-05 00:17:09 +0600

    added solution of parse search

--- Sorting and Searching/Search in Rotated Array/SearchInRotatedArray.js
+++ Sorting and Searching/Search in Rotated Array/SearchInRotatedArray.js
@@ -1,5 +1,6 @@
 const Search = (array, l, h, key) =>
 {
+    console.log(`this is l ${l} and this is h ${h}`)
     if (l > h) return -1
 
     let mid = (l + h) / 2
@@ -9,13 +10,15 @@ const Search = (array, l, h, key) =>
     if (array[l] <= array[mid])
     {
         if (key >= array[l] && key <= array[mid])
+        {
             return Search(array, l, mid - 1, key)
-        
+        }
         return Search(array, mid+1, h, key)
     }
     if (key >= array[mid] && key <= array[h])
+    {
         return Search(array, mid + 1, h, key)
-    
+    }    
     return Search(array, l, mid-1, key)
 }
 
@@ -23,12 +26,12 @@ let array = [4, 5, 6, 7, 8, 9, 1, 2, 3]
 let length = array.length
 console.log(length)
 let key = 9
-let i = Search(array, 0, length - 1, key)
-if (i != -1)
+let i = Search(array, 0, length, key)
+if (i == -1)
 {
-    console.log(`Index ${i}`)
+    console.log(`Key not found`)
 }
 else
 {
-    console.log(`Key not found`)
+    console.log(`Found`)
 }
---
+++ Sorting and Searching/Sparse Search/SparseSearch.js
@@ -0,0 +1,65 @@
+const binarySearch = (array, low, high, string) =>
+{
+    if (low > high)
+    {
+        return -1
+    }
+    let mid = (low + high) / 2
+    if (array[mid] == "")
+    {
+        let left = mid - 1
+        let right = mid + 1
+
+        while (true)
+        {
+            if (left < low && right > high)
+            {
+                return -1
+            }
+            if (left >= low && array[left] != "")
+            {
+                mid = left
+                break
+            }
+            else if (right <= high && array[right] != "")
+            {
+                mid = right
+                break
+            }
+            left--
+            right++
+            }
+    }
+    if (array[mid] == string)
+    {
+        return mid
+    }
+    else if (array[mid] > string)
+    {
+        return binarySearch(array, low, mid - 1, string)
+    }
+    else
+    {
+        return binarySearch(array, mid + 1, high, string)    
+    }
+}
+
+const sparseSearch = (array, string, n) =>
+{
+    return binarySearch(array, 0, n - 1, string)
+}
+
+const array = ["for", "geeks", "", "", "", "", "ide", "practice", "", "", "", "quiz"]
+const x = "geeks"
+const n = x.length
+let index = sparseSearch(array, x, n)
+if (index != -1)
+{
+    console.log(`${x} is found at index ${index}`)
+}
+else
+{
+    console.log(`${x} is not found`)
+}
+    
+
