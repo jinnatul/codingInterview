d40b28e52dccd10a3b0165958f176b79e8fa5d17
zubayerhimel <zhimel007@gmail.com>
2020-02-06 01:30:14 +0600

    solution of sorted matrix search

---
+++ Sorting and Searching/Sorted Matrix Search/SortedMatrixSearch.js
@@ -0,0 +1,29 @@
+const search = (matrix, n, number) => {
+    if (n == 0) {
+        return -1
+    }
+    if (number < matrix[0][0] || number > matrix[n - 1][n - 1]) {
+        return -1
+    }
+    let i = 0, j = n - 1
+    while (i < n && j >= 0) {
+        if (matrix[i][j] == number) {
+            return console.log(`${number} is found at ${i} ${j}`)
+        }
+        if (matrix[i][j] > number) {
+            j--
+        }
+        else {
+            i++
+        }
+    }
+    return console.log(`${number} is not found`)
+}
+
+const matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [12, 23, 32, 43],
+    [11, 22, 34, 42],
+];
+search(matrix, 4, 7)
